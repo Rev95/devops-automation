9a47ba6708c81cc6795f2b3405627612888ebcee
Revanth <tummala.sairevanth@gmail.com>
2024-12-13 00:19:36 -0600

    pipeline check

--- src/test/java/com/dev/spring_boot_app/service/OrderServiceTests.java
+++ src/test/java/com/dev/spring_boot_app/service/OrderServiceTests.java
@@ -37,7 +37,7 @@ public class OrderServiceTests {
         Order result = orderService.fetchOrder(orderId);
 
         // Assert
-        assertNotNull(result);
+        assertNotNull(result);  // adding this comment to check pipeline
         assertEquals(orderId, result.getOrderId());
         assertEquals("2023-12-11", result.getOrderDate());
         assertEquals("500", result.getTotalAmount());
